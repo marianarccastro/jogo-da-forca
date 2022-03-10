3198678979c85726f46825f65e02d2e9011db0b2
Mariana <marianarccastro@gmail.com>
2022-03-09 23:49:54 -0300

    Adicionando funções

--- main.js
+++ main.js
@@ -1,65 +1,71 @@
 
-let palavras = ["algoritmo", "alicate", "dinossauro", "computador", "girafa", "mochila", "impressora", "tradutor", "porcelana", "frigideira"];
-let palavraSorteada = palavras[Math.floor(Math.random()*palavras.length)].toUpperCase();
-
-console.log(palavraSorteada);
-
-//cria os espaços onde ficará a palavra
-for (let i = 0; i < palavraSorteada.length; i++){
-	let espaco = document.createElement('p');
-	espaco.innerHTML = '_';
-	document.querySelector('#palavra-escondida').appendChild(espaco);
-	espaco.setAttribute('id',i);
-}
-
+const palavras = ["algoritmo", "alicate", "dinossauro", "computador", "girafa", "mochila", "impressora", "tradutor", "porcelana", "frigideira"];
+const palavraSorteada = palavras[Math.floor(Math.random()*palavras.length)].toUpperCase();
 let letrasAcertadas = 0;
-let letraEscondida = '';
+let letraEscondida;
+let letraEscolhida;
 let tentativas = 7;
 let imagem = document.querySelector('#imagem-forca')
+const resultado = document.querySelector('#resultado');
 const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas.forEach( tecla => tecla.onclick = function(){verifica(tecla)});
 
-for (let i = 0; i < listaDeTeclas.length; i++){
-	let tecla = listaDeTeclas[i];
-	tecla.onclick = function () {
-		verifica (tecla);
-	}	
+console.log(palavraSorteada);
+criaEspacoPalavra();
+
+function criaEspacoPalavra(){
+	for (let i = 0; i < palavraSorteada.length; i++){
+		let espaco = document.createElement('p');
+		espaco.innerHTML = '_';
+		document.querySelector('#palavra-escondida').appendChild(espaco);
+		espaco.setAttribute('id',i);
+	}
 }
 
-function verifica(tecla){
-	let letraEscolhida = tecla.value;
-	let acertouLetra = false;
-
+function imprimeLetra(){
 	for (let i = 0; i < palavraSorteada.length; i++){
 		if (letraEscolhida == palavraSorteada.charAt(i)){
 			letraEscondida = document.getElementById(i);
 			letraEscondida.innerHTML = palavraSorteada.charAt(i);
-			acertouLetra = true;
 			letrasAcertadas++;
 		}
 	}
-	
-	if (!acertouLetra && tentativas > 0){
-		tentativas--;
-		let novoSrcImg = `imagens/forca${tentativas}.png`
-		imagem.setAttribute('src', novoSrcImg);
-	}
+}
 
-	if (tentativas == 0){
-		const resultado = document.querySelector('#resultado');
-		resultado.innerHTML = `Suas tentativas acabaram! A palavra é ${palavraSorteada}`;
+function desenhaForca(){
+	tentativas--;
+	let novoSrcImg = `imagens/forca${tentativas}.png`
+	imagem.setAttribute('src', novoSrcImg);
+}
+
+function fimDoJogo(){
+	if (letrasAcertadas == palavraSorteada.length || tentativas == 0){
 		listaDeTeclas.forEach(botao => botao.setAttribute('disabled','true'));
+		return true;
 	}
+}
 
+function imprimeResultado(){
 	if (letrasAcertadas == palavraSorteada.length){
-		const resultado = document.querySelector('#resultado');
 		resultado.innerHTML = 'Parabéns!!! Você acertou!';
-		listaDeTeclas.forEach(botao => botao.setAttribute('disabled','true'));
+	} else if (tentativas == 0){
+		resultado.innerHTML = `Suas tentativas acabaram! A palavra é ${palavraSorteada}`;
 	}
-	
+}
+
+function verifica(tecla){
+	letraEscolhida = tecla.value;
 	tecla.setAttribute('disabled','true');
-	if (acertouLetra){
+
+	if (palavraSorteada.includes(letraEscolhida)){
+		imprimeLetra();
 		tecla.classList.add('tecla_acertou');
 	} else {
+		desenhaForca();
 		tecla.classList.add('tecla_errou');
-	}	
+	}
+
+	if (fimDoJogo()){
+		imprimeResultado();
+	}
 }
